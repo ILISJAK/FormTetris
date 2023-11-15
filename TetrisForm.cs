0aeebb7aa2201a7c49b7f7ef6ac95579fb7b2516
Ivan Lisjak <ilisjak@vub.hr>
2023-11-15 21:17:47 +0100

    Initial commit

---
+++ TetrisForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace FormTetris
+{
+    public partial class TetrisForm : Form
+    {
+        public TetrisForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
